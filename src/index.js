e25214f515d0cf0eeeefd2fc6799c74ce78b10fb
Stuart Hamblin <stuart@Stuarts-MacBook-Air.local>
2018-11-02 10:38:12 -0500

    add some lecture notes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
-// sayHello = () => {
-//     console.log("Hello")
-// };
-// sayHello();
+sayHello = () => {
+    console.log("Hello")
+};
+sayHello();
 
 const $ = require('jquery');
+const = require('')
 
 $('body').append("<h2>JQUERY has successfully installed</h2>");
